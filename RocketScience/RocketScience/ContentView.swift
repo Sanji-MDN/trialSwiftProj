784a2b0cc50587484ada2ff8ba81e66ee81d1cfd
Sanji-MDN <159733465+Sanji-MDN@users.noreply.github.com>
2024-05-01 20:05:49 +0200

    Add files via upload

---
+++ RocketScience/RocketScience/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RocketScience
+//
+//  Created by Mario Di Nitto on 01/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RocketScience/RocketScience/RocketScienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RocketScienceApp.swift
+//  RocketScience
+//
+//  Created by Mario Di Nitto on 01/05/24.
+//
+
+import SwiftUI
+
+@main
+struct RocketScienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
